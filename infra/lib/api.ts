448392b592d277557b54a5c8807dc001401d8686
David J Drake <95321084+DavidJDrake@users.noreply.github.com>
2026-09-01 18:49:48 -0400

    feat(infra): HTTP API with JWT authorizer, download Lambda, and downloads table

---
+++ infra/lib/api.ts
@@ -0,0 +1,70 @@
+import { Duration, RemovalPolicy, Stack } from "aws-cdk-lib";
+import * as apigw from "aws-cdk-lib/aws-apigatewayv2";
+import { HttpJwtAuthorizer } from "aws-cdk-lib/aws-apigatewayv2-authorizers";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+import * as path from "node:path";
+import { CONFIG } from "./config";
+
+export interface ApiProps {
+  userPool: cognito.IUserPool;
+  client: cognito.IUserPoolClient;
+  booksBucket: s3.IBucket;
+  siteBucket: s3.IBucket;
+}
+
+export class Api extends Construct {
+  readonly httpApi: apigw.HttpApi;
+  readonly table: dynamodb.Table;
+
+  constructor(scope: Construct, id: string, props: ApiProps) {
+    super(scope, id);
+
+    this.table = new dynamodb.Table(this, "Downloads", {
+      partitionKey: { name: "email", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "sk", type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+
+    const downloadFn = new NodejsFunction(this, "DownloadFn", {
+      entry: path.join(__dirname, "../lambda/download/index.ts"),
+      runtime: lambda.Runtime.NODEJS_22_X,
+      timeout: Duration.seconds(10),
+      memorySize: 256,
+      environment: {
+        BOOKS_BUCKET: props.booksBucket.bucketName,
+        SITE_BUCKET: props.siteBucket.bucketName,
+        DOWNLOADS_TABLE: this.table.tableName,
+      },
+    });
+    props.booksBucket.grantRead(downloadFn);
+    props.siteBucket.grantRead(downloadFn, "catalog.json");
+    this.table.grantWriteData(downloadFn);
+
+    const authorizer = new HttpJwtAuthorizer("Jwt", `https://cognito-idp.${Stack.of(this).region}.amazonaws.com/${props.userPool.userPoolId}`, {
+      jwtAudience: [props.client.userPoolClientId],
+    });
+
+    this.httpApi = new apigw.HttpApi(this, "HttpApi", {
+      defaultAuthorizer: authorizer,
+      corsPreflight: {
+        allowOrigins: [`https://${CONFIG.siteDomain}`, CONFIG.localDevOrigin],
+        allowMethods: [apigw.CorsHttpMethod.POST, apigw.CorsHttpMethod.OPTIONS],
+        allowHeaders: ["authorization", "content-type"],
+        maxAge: Duration.hours(1),
+      },
+    });
+
+    this.httpApi.addRoutes({
+      path: "/download",
+      methods: [apigw.HttpMethod.POST],
+      integration: new HttpLambdaIntegration("DownloadIntegration", downloadFn),
+    });
+  }
+}
---
+++ infra/test/api.test.ts
@@ -0,0 +1,72 @@
+import { App, Stack } from "aws-cdk-lib";
+import { Match, Template } from "aws-cdk-lib/assertions";
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { describe, it } from "vitest";
+import { Api } from "../lib/api";
+import { CONFIG } from "../lib/config";
+
+function synth() {
+  const stack = new Stack(new App(), "Test", { env: { account: "123456789012", region: "us-east-1" } });
+  const userPool = new cognito.UserPool(stack, "Pool");
+  const client = userPool.addClient("Client");
+  new Api(stack, "Api", {
+    userPool, client,
+    booksBucket: new s3.Bucket(stack, "Books"),
+    siteBucket: new s3.Bucket(stack, "Site"),
+  });
+  return Template.fromStack(stack);
+}
+
+describe("Api", () => {
+  it("creates a retained on-demand downloads table keyed by email/sk", () => {
+    const t = synth();
+    t.hasResource("AWS::DynamoDB::Table", {
+      DeletionPolicy: "Retain",
+      Properties: Match.objectLike({
+        BillingMode: "PAY_PER_REQUEST",
+        KeySchema: [
+          { AttributeName: "email", KeyType: "HASH" },
+          { AttributeName: "sk", KeyType: "RANGE" },
+        ],
+      }),
+    });
+  });
+
+  it("wires the download Lambda with bucket and table env vars", () => {
+    const t = synth();
+    t.hasResourceProperties("AWS::Lambda::Function", {
+      Runtime: "nodejs22.x",
+      Environment: { Variables: Match.objectLike({
+        BOOKS_BUCKET: Match.anyValue(), SITE_BUCKET: Match.anyValue(), DOWNLOADS_TABLE: Match.anyValue(),
+      }) },
+    });
+    t.hasResourceProperties("AWS::IAM::Policy", {
+      PolicyDocument: Match.objectLike({ Statement: Match.arrayWith([
+        Match.objectLike({ Action: Match.arrayWith(["s3:GetObject*"]) }),
+        Match.objectLike({ Action: Match.arrayWith(["dynamodb:PutItem"]) }),
+      ]) }),
+    });
+  });
+
+  it("exposes POST /download behind a Cognito JWT authorizer with CORS for the site and localhost", () => {
+    const t = synth();
+    t.hasResourceProperties("AWS::ApiGatewayV2::Api", {
+      ProtocolType: "HTTP",
+      CorsConfiguration: Match.objectLike({
+        AllowOrigins: [`https://${CONFIG.siteDomain}`, CONFIG.localDevOrigin],
+        AllowMethods: Match.arrayWith(["POST"]),
+        AllowHeaders: Match.arrayWith(["authorization", "content-type"]),
+      }),
+    });
+    t.hasResourceProperties("AWS::ApiGatewayV2::Authorizer", {
+      AuthorizerType: "JWT",
+      IdentitySource: ["$request.header.Authorization"],
+      JwtConfiguration: Match.objectLike({ Audience: [Match.anyValue()] }),
+    });
+    t.hasResourceProperties("AWS::ApiGatewayV2::Route", {
+      RouteKey: "POST /download",
+      AuthorizationType: "JWT",
+    });
+  });
+});
